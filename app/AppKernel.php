de8b1b7a42d7fb70b117205f41877e443b5074bb
peillis <enrique@markhaus.com>
2011-08-19 21:22:07 +0200

    SiwappBundle added

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new Markhaus\SiwappBundle\MarkhausSiwappBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Markhaus/SiwappBundle/MarkhausSiwappBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Markhaus\SiwappBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MarkhausSiwappBundle extends Bundle
+{
+}

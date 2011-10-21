800720f09bb72476ab6ebc59a2bb02ba3225b208
Carlos Escribano <carlos@markhaus.com>
2011-10-21 12:50:03 +0200

    Estimates base actions and some views.
    Signed-off-by: Carlos Escribano <carlos@markhaus.com>

--- src/Siwapp/EstimateBundle/Controller/DefaultController.php
+++ src/Siwapp/EstimateBundle/Controller/DefaultController.php
@@ -4,16 +4,75 @@ namespace Siwapp\EstimateBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+/**
+ * @Route("/estimates")
+ */
 class DefaultController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
-     * @Template()
+     * @Route("/", name="estimate_index")
+     * @Template
      */
-    public function indexAction($name)
+    public function indexAction()
     {
-        return array('name' => $name);
+        return array();
+    }
+    
+    /**
+     * @Route("/show", name="estimate_show")
+     * @Template
+     */
+    public function showAction()
+    {
+        return array();
+    }
+    
+    /**
+     * @Route("/add", name="estimate_add")
+     * @Template("SiwappEstimateBundle:Default:edit.html.twig")
+     */
+    public function addAction()
+    {
+        return array();
+    }
+    
+    /**
+     * @Route("/create", name="estimate_create")
+     * @Method("POST")
+     * @Template("SiwappEstimateBundle:Default:edit.html.twig")
+     */
+    public function createAction()
+    {
+        return $this->redirect($this->generateUrl('estimate_edit'));
+    }
+    
+    /**
+     * @Route("/edit", name="estimate_edit")
+     * @Template
+     */
+    public function editAction()
+    {
+        return array();
+    }
+    
+    /**
+     * @Route("/update", name="estimate_update")
+     * @Method("POST")
+     * @Template("SiwappEstimateBundle:Default:edit.html.twig")
+     */
+    public function updateAction()
+    {
+        return $this->redirect($this->generateUrl('estimate_edit'));
+    }
+    
+    /**
+     * @Route("/delete", name="estimate_delete")
+     */
+    public function deleteAction()
+    {
+        return $this->redirect($this->generateUrl('estimate_index'));
     }
 }

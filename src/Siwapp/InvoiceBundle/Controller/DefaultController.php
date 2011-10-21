de75c879d16751eb3f78b61b66291137798ca82b
Carlos Escribano <carlos@markhaus.com>
2011-10-21 12:36:17 +0200

    Default views for InvoiceBundle
    Signed-off-by: Carlos Escribano <carlos@markhaus.com>

--- src/Siwapp/InvoiceBundle/Controller/DefaultController.php
+++ src/Siwapp/InvoiceBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace Siwapp\InvoiceBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 use Siwapp\InvoiceBundle\Form\InvoiceType;
@@ -14,7 +15,7 @@ use Siwapp\InvoiceBundle\Form\InvoiceType;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="invoice_index")
      * @Template
      */
     public function indexAction()
@@ -23,7 +24,38 @@ class DefaultController extends Controller
     }
     
     /**
-     * @Route("/edit")
+     * @Route("/show", name="invoice_show")
+     * @Template
+     */
+    public function showAction()
+    {
+        return array();
+    }
+    
+    /**
+     * @Route("/add", name="invoice_add")
+     * @Template("SiwappInvoiceBundle:Default:edit.html.twig")
+     */
+    public function addAction()
+    {
+        $form = $this->createForm(new InvoiceType());
+        return array(
+            'form' => $form->createView(),
+        );
+    }
+    
+    /**
+     * @Route("/create", name="invoice_create")
+     * @Method("POST")
+     * @Template("SiwappInvoiceBundle:Default:edit.html.twig")
+     */
+    public function createAction()
+    {
+        return $this->redirect($this->generateUrl('invoice_edit'));
+    }
+    
+    /**
+     * @Route("/edit", name="invoice_edit")
      * @Template
      */
     public function editAction()
@@ -32,5 +64,23 @@ class DefaultController extends Controller
         return array(
             'form' => $form->createView(),
         );
+    }
+    
+    /**
+     * @Route("/update", name="invoice_update")
+     * @Method("POST")
+     * @Template("SiwappInvoiceBundle:Default:edit.html.twig")
+     */
+    public function updateAction()
+    {
+        return $this->redirect($this->generateUrl('invoice_edit'));
+    }
+    
+    /**
+     * @Route("/delete", name="invoice_delete")
+     */
+    public function deleteAction()
+    {
+        return $this->redirect($this->generateUrl('invoice_index'));
     }
 }

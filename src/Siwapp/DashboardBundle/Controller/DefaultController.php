798bf4308e4a50517eeec016eff887513297516b
peillis <enrique@markhaus.com>
2011-11-18 17:34:31 +0100

    fixing dashboard presentation

--- src/Siwapp/DashboardBundle/Controller/DefaultController.php
+++ src/Siwapp/DashboardBundle/Controller/DefaultController.php
@@ -14,55 +14,13 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        $invoices = array(
-            array(
-                'id' => '2',
-                'number' => 'ASET-02',
-                'customer' => 'Roxxon',
-                'date' => '05/28/2011',
-                'duedate' => '09/16/2011',
-                'status' => array('', 'draft'),
-                'due' => '',
-                'total' => '$11,435.23',
-            ),
-            array(
-                'id' => '3',
-                'number' => 'ASET-03',
-                'customer' => 'Roxxon',
-                'date' => '05/28/2011',
-                'duedate' => '09/16/2011',
-                'status' => array('pending', 'pending'),
-                'due' => '$9,000.00',
-                'total' => '$11,435.23',
-            ),
-            array(
-                'id' => '4',
-                'number' => 'ASET-04',
-                'customer' => 'Roxxon',
-                'date' => '05/28/2011',
-                'duedate' => '09/16/2011',
-                'status' => array('paid', 'paid'),
-                'due' => '',
-                'total' => '$11,435.23',
-            ),
-        );
+        $em = $this->getDoctrine()->getEntityManager();
         
-        $overdue = array(
-            array(
-                'id' => '1',
-                'number' => 'ASET-01',
-                'customer' => 'Roxxon',
-                'date' => '05/28/2011',
-                'duedate' => '09/16/2011',
-                'status' => array('overdue', 'overdue'),
-                'due' => '$4,000.00',
-                'total' => '$11,435.23',
-            ),
-        );
+        $entities = $em->getRepository('SiwappInvoiceBundle:Invoice')->findAll();
         
         return array(
-            'invoices' => $invoices,
-            'overdue_invoices' => $overdue,
+            'invoices' => array_slice($entities, 0, 4),
+            'overdue_invoices' => array_slice($entities, 0, 4)
         );
     }
 }

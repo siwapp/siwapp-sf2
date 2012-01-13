da924879f4edd67ec44898c5a0ccdcb55dddcc7d
peillis <enrique@markhaus.com>
2012-01-13 13:06:18 +0100

    added Property entity

---
+++ src/Siwapp/ConfigBundle/Entity/Property.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Siwapp\ConfigBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Siwapp\ConfigBundle\Entity\Property
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Siwapp\ConfigBundle\Entity\PropertyRepository")
+ */
+class Property
+{
+    /**
+     * @var string $keey
+     *
+     * @ORM\Column(name="keey", type="string", length=50, unique=true)
+     * @ORM\Id
+     */
+    private $keey;
+
+    /**
+     * @var text $value
+     *
+     * @ORM\Column(name="value", type="text")
+     */
+    private $value;
+
+    /**
+     * Set keey
+     *
+     * @param string $keey
+     */
+    public function setKeey($keey)
+    {
+        $this->keey = $keey;
+    }
+
+    /**
+     * Get keey
+     *
+     * @return string 
+     */
+    public function getKeey()
+    {
+        return $this->keey;
+    }
+
+    /**
+     * Set value
+     *
+     * @param text $value
+     */
+    public function setRawValue($value)
+    {
+        $this->value = $value;
+    }
+    
+    /**
+     * Set value with JSON encode
+     *
+     * @param text $value
+     * @author Enrique Martinez
+     **/
+    public function setValue($value)
+    {
+        $this->setRawValue(json_encode($value));
+    }
+
+    /**
+     * Get value
+     *
+     * @return text 
+     */
+    public function getRawValue()
+    {
+        return $this->value;
+    }
+    
+    /**
+     * returns the value JSON decoded
+     *
+     * @return json
+     * @author Enrique Martinez
+     **/
+    public function getValue()
+    {
+        return json_decode($this->getRawValue());
+    }
+}
